1636902897358c4dd263b71576b89a36b6c301e9
Phillip <patransd@berkeley.edu>
2019-01-29 15:09:38 -0800

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.get('/todos', (req, res) => {
 	});
 });
 
-
+	
 // GET /todos/1234324
 app.get('/todos/:id', (req, res) => {
 	var id = req.params.id;
@@ -53,8 +53,26 @@ app.get('/todos/:id', (req, res) => {
 	});
 });
 
+//REMOVE /todos/32131
+app.delete('/todos/:id', (req, res) => {
+	var id = req.params.id;
+
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+	Todo.findByIdAndRemove(id).then((todo) =>{
+		if (!todo) {
+			return res.status(404).send();
+		}
+		res.send(todo);
+	}).catch((e) => {
+		res.status(400).send();
+	});
+});
+
 app.listen(port, () => {
 	console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
+
